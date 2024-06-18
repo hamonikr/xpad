7c5982cbbf2086b1f4460379b9d696c0cc4766a3
Kevin Kim <chaeya@gmail.com>
2024-06-18 09:21:49 +0900

    Show tray icon in ubuntu 24.04 Wayland

--- src/xpad-tray.c
+++ src/xpad-tray.c
@@ -3,6 +3,7 @@
 Copyright (c) 2002 Jamis Buck
 Copyright (c) 2003-2007 Michael Terry
 Copyright (c) 2013-2014 Arthur Borsboom
+Copyright (c) 2022-2024 Kevin Kim
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -26,10 +27,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <gtk/gtk.h>
 
 #include "xpad-settings.h"
-
-#ifdef HAVE_APP_INDICATOR
-#include <libayatana-appindicator/app-indicator.h>
-
 #include "xpad-tray.h"
 #include "xpad-app.h"
 #include "xpad-pad.h"
@@ -49,7 +46,8 @@ enum
 #define ICON_NAME "xpad"
 #define TRAY_ICON "xpad-panel"
 
-static AppIndicator *app_indicator = NULL;
+static GtkStatusIcon *status_icon = NULL;
+static GtkWidget *tray_menu = NULL;
 
 static void menu_spawn (XpadSettings *settings)
 {
@@ -125,58 +123,65 @@ static char const* getIconName(void)
     return icon_name;
 }
 
-static AppIndicator* xpad_tray_app_indicator_new (XpadSettings *settings)
+static GtkStatusIcon* xpad_tray_status_icon_new (XpadSettings *settings)
 {
 	char const* icon_name = getIconName();
-	AppIndicator* indicator = app_indicator_new(ICON_NAME, icon_name, APP_INDICATOR_CATEGORY_SYSTEM_SERVICES);
-	app_indicator_set_status(indicator, APP_INDICATOR_STATUS_ACTIVE);
-	app_indicator_set_title(indicator, g_get_application_name());
-	return indicator;
+	GtkStatusIcon* icon = gtk_status_icon_new_from_icon_name(icon_name);
+	gtk_status_icon_set_title(icon, g_get_application_name());
+	gtk_status_icon_set_visible(icon, TRUE);
+	return icon;
 }
 
-/* Create a fingerprint of all the  pad titles. */
+/* Create a fingerprint of all the pad titles. */
 static gchar* xpad_tray_get_fingerprint () {
-        /* Get all pads sorted by title. */
-        GSList *pads = xpad_pad_group_get_pads_sorted_by_title(xpad_app_get_pad_group ());
+    /* Get all pads sorted by title. */
+    GSList *pads = xpad_pad_group_get_pads_sorted_by_title(xpad_app_get_pad_group ());
 
-        GString *pads_fingerprint = g_string_new(NULL);
+    GString *pads_fingerprint = g_string_new(NULL);
 
-        for (gint n = 1; pads; pads = pads->next, n++) {
-                XpadPad *pad = pads->data;
-                gchar *title = xpad_pad_get_title_for_menu(pad, n);
-                g_string_append(pads_fingerprint, title);
-                g_free (title);
-        }
+    for (gint n = 1; pads; pads = pads->next, n++) {
+        XpadPad *pad = pads->data;
+        gchar *title = xpad_pad_get_title_for_menu(pad, n);
+        g_string_append(pads_fingerprint, title);
+        g_free (title);
+    }
 
-        g_slist_free (pads);
+    g_slist_free (pads);
 
-        /* Return the fingerprint. */
-        return g_string_free (pads_fingerprint, FALSE);
+    /* Return the fingerprint. */
+    return g_string_free (pads_fingerprint, FALSE);
 }
 
 static gboolean xpad_tray_update_menu (XpadSettings *settings) {
-	GtkMenu *current_menu = app_indicator_get_menu (app_indicator);
+	/* Determine if the menu items have changed. */
+	gchar *current_fingerprint = g_object_get_data (G_OBJECT (tray_menu), "pads-fingerprint");
+	gchar *new_fingerprint = xpad_tray_get_fingerprint();
 
-	/* If there is no menu, then add it. */
-	if (current_menu == NULL) {
-		GtkWidget* new_menu = xpad_tray_create_menu(settings);
-		app_indicator_set_menu(app_indicator, GTK_MENU(new_menu));
-	} else {
-		/* Determine if the menu items have changed. */
-		gchar *current_fingerprint = g_object_get_data (G_OBJECT (current_menu), "pads-fingerprint");
-		gchar *new_fingerprint = xpad_tray_get_fingerprint();
+	int menu_changed = g_strcmp0 (current_fingerprint, new_fingerprint);
+	g_free (new_fingerprint);
 
-		int menu_changed = g_strcmp0 (current_fingerprint, new_fingerprint);
-		g_free (new_fingerprint);
-
-		/* If the menu did change, then set the new menu. */
-		if (menu_changed != 0) {
-			GtkWidget* new_menu = xpad_tray_create_menu(settings);
-			app_indicator_set_menu(app_indicator, GTK_MENU(new_menu));
+	/* If the menu did change, then set the new menu. */
+	if (menu_changed != 0) {
+		if (tray_menu) {
+			gtk_widget_destroy(tray_menu);
 		}
+		tray_menu = xpad_tray_create_menu(settings);
+		g_object_set_data_full(G_OBJECT(tray_menu), "pads-fingerprint", g_strdup(new_fingerprint), g_free);
 	}
 
 	return TRUE;
+}
+
+static void tray_icon_on_click(GtkStatusIcon *status_icon, guint button, guint activate_time, gpointer user_data)
+{
+    XpadSettings *settings = (XpadSettings *)user_data;
+    if (tray_menu) {
+        gtk_menu_popup_at_pointer(GTK_MENU(tray_menu), NULL);
+    } else {
+        tray_menu = xpad_tray_create_menu(settings);
+        g_object_set_data_full(G_OBJECT(tray_menu), "pads-fingerprint", xpad_tray_get_fingerprint(), g_free);
+        gtk_menu_popup_at_pointer(GTK_MENU(tray_menu), NULL);
+    }
 }
 
 void xpad_tray_init (XpadSettings *settings) {
@@ -184,39 +189,25 @@ void xpad_tray_init (XpadSettings *settings) {
     g_object_get (settings, "tray-enabled", &tray_enabled, NULL);
 
     if (tray_enabled) {
-        app_indicator = xpad_tray_app_indicator_new(settings);
-	xpad_tray_update_menu(settings);
+        status_icon = xpad_tray_status_icon_new(settings);
+        g_signal_connect(status_icon, "activate", G_CALLBACK(tray_icon_on_click), settings);
+        g_signal_connect(status_icon, "popup-menu", G_CALLBACK(tray_icon_on_click), settings);
 
-	/* Refresh the menu every x seconds */
-	guint refresh_each_seconds = 10;
-	g_timeout_add_seconds(refresh_each_seconds, (GSourceFunc) xpad_tray_update_menu, settings);
+		/* Refresh the menu every x seconds */
+		guint refresh_each_seconds = 10;
+		g_timeout_add_seconds(refresh_each_seconds, (GSourceFunc) xpad_tray_update_menu, settings);
     }
 }
 
 void xpad_tray_dispose (XpadSettings *settings) {
-    if (app_indicator)
-        g_clear_object (&app_indicator);
+    if (status_icon)
+        g_object_unref (status_icon);
 }
 
 gboolean xpad_tray_has_indicator ()
 {
-	if (app_indicator)
+	if (status_icon)
 		return TRUE;
 	else
 		return FALSE;
 }
-
-#else
-
-void xpad_tray_init (XpadSettings *settings) {
-}
-
-void xpad_tray_dispose (XpadSettings *settings) {
-}
-
-gboolean xpad_tray_has_indicator ()
-{
-	return FALSE;
-}
-
-#endif
